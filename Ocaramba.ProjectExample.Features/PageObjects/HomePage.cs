a793f624a27fd1fae60c122254fb4b8f91f13f20
Pawel K <pawel.kreski@yahoo.com>
2020-04-21 22:46:05 +0200

    Groundwork, first test (#1)
    Co-authored-by: Pawel Kreski <pawel.kreski@yahoo.com>

---
+++ Ocaramba.ProjectExample.Features/PageObjects/HomePage.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="HomePage.cs" company="Objectivity Bespoke Software Specialists">
+// Copyright (c) Objectivity Bespoke Software Specialists. All rights reserved.
+// </copyright>
+// <license>
+//     The MIT License (MIT)
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of this software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//     The above copyright notice and this permission notice shall be included in all
+//     copies or substantial portions of the Software.
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//     SOFTWARE.
+// </license>
+
+using System;
+using System.Globalization;
+using NLog;
+using Ocaramba;
+using Ocaramba.Extensions;
+using Ocaramba.Types;
+
+namespace OmadaSampleTestsProject.PageObjects
+{
+    public class HomePage : ProjectPageBase
+    {
+        private static readonly NLog.Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public readonly ElementLocator cookieCloseBtn = new ElementLocator(Locator.CssSelector, "[data-init='cookiebar'] span");
+        public readonly ElementLocator topNavigationSection = new ElementLocator(Locator.CssSelector, "[data-init='top-navigation']");
+        public readonly ElementLocator footerSection = new ElementLocator(Locator.CssSelector, "[data-init='footer']");
+        public readonly ElementLocator searchBar = new ElementLocator(Locator.CssSelector, ".header__search");
+
+        public HomePage(DriverContext driverContext)
+            : base(driverContext)
+        {
+        }
+
+        public void CloseCookieBar()
+        {
+            Driver.GetElement(cookieCloseBtn).Click();
+        }
+
+        public HomePage OpenHomePage()
+        {
+            var url = BaseConfiguration.GetUrlValue;
+            Logger.Info(CultureInfo.CurrentCulture, "Opening page {0}", url);
+            this.Driver.NavigateTo(new Uri(url));
+            CloseCookieBar();
+
+            return this;
+        }
+    }
+}
